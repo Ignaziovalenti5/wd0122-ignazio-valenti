2ae8db81cb5077d407ab418c991adb7a62d861bb
Ignaziovalenti5 <ignazio.valenti.87@gmail.com>
2022-04-16 22:09:53 +0200

    m1 w3 d5

--- m1/w3/d5 - progetto/calculator/assets/js/script.js
+++ m1/w3/d5 - progetto/calculator/assets/js/script.js
@@ -1,75 +1,40 @@
 const DISPLAY = document.querySelector('#display-input')
 const HISTORY = document.querySelector('#history p')
 
+function isOperator(o) {
+    return (o == '+' || o == '-' || o == '*' || o == '/')
+}
+
+function operatorIsPresent() {
+    let num = DISPLAY.value
+    return (num.includes("+") || num.includes("-") ||num.includes("*") || num.includes("/") )
+}
+
+function isComma(c) {
+    return (c == '.')
+}
+
+function commaIsPresent() {
+    let num = DISPLAY.value
+    return ( num.includes('.') )
+}
+
 function addNumber(n) {
-    number = n.getAttribute('data-number')
-    
-    if((number == ".") && (DISPLAY.value.includes("."))){
-        DISPLAY.value =  '.'
-        HISTORY.innerHTML = '.'
-    }else{
-        DISPLAY.value += number
-        HISTORY.innerHTML += number
+    if( isOperator(n) && operatorIsPresent() ){
+        result()
+    }
+    if( !(isOperator(n) && operatorIsPresent()) && (!( isComma(n) && commaIsPresent()) ) ){
+        DISPLAY.value += n
+        HISTORY.innerHTML += n
     }
 }
 
-function multiplication(o) {
-    op = o.getAttribute('data-operation')
-    HISTORY.innerHTML = eval(HISTORY.innerHTML) + op
-    DISPLAY.value = HISTORY.innerHTML
-    
-    if(DISPLAY.value == undefined || HISTORY.innerHTML == (undefined + op)){
-        DISPLAY.value = ''
-        HISTORY.innerHTML = ''
-    }
-}
-
-function division(o) {
-    op = o.getAttribute('data-operation')
-    HISTORY.innerHTML = eval(HISTORY.innerHTML) + op
-    DISPLAY.value = HISTORY.innerHTML
-    
-    if(DISPLAY.value == undefined || HISTORY.innerHTML == (undefined + op)){
-        DISPLAY.value = ''
-        HISTORY.innerHTML = ''
-    }
-}
-
-function subtraction(o) {
-    op = o.getAttribute('data-operation')
-    HISTORY.innerHTML = eval(HISTORY.innerHTML) + op
-    DISPLAY.value = HISTORY.innerHTML
-    
-    // puoi iniziare un numero col segno " - "
-    if(DISPLAY.value == undefined || HISTORY.innerHTML == (undefined + op)){
-        DISPLAY.value = '-'
-        HISTORY.innerHTML = '-'
-    }
-}
-
-function addiction(o) {
-    op = o.getAttribute('data-operation')
-    HISTORY.innerHTML = eval(HISTORY.innerHTML) + op
-    DISPLAY.value = HISTORY.innerHTML
-
-    // puoi iniziare un numero col segno " + "
-    if(DISPLAY.value == undefined || HISTORY.innerHTML == (undefined + op)){
-        DISPLAY.value = '+'
-        HISTORY.innerHTML = '+'
-    }
-}
-
-function square() { // per elevare alla potenza
-    let total = Math.pow(DISPLAY.value, 2)
+function result() {
+    let total = eval(DISPLAY.value)
     DISPLAY.value = total
-    HISTORY.innerHTML = total
-}
+    HISTORY.innerHTML = total + '<br>'
 
-function result(){
-    let total = eval(HISTORY.innerHTML)
-    DISPLAY.value = total
-    
-    if(total == undefined){ // se si clicca su uguale allo start
+    if(total == undefined){
         DISPLAY.value = ''
         HISTORY.innerHTML = ''
     }else if(total == Infinity){ // divisione per " 0 "
@@ -77,17 +42,21 @@ function result(){
     }
 }
 
-function reset(){
+function reset() {
     DISPLAY.value = ''
     HISTORY.innerHTML = ''
 }
 
-function deleteOne() { // funzione backspace
+function backSpace() {
     let del = DISPLAY.value
     DISPLAY.value = del.slice(0, del.length-1)
     HISTORY.innerHTML = del.slice(0, del.length-1)
 }
 
-
+function square() {
+    let total = Math.pow(DISPLAY.value, 2)
+    DISPLAY.value = total
+    HISTORY.innerHTML = total
+}
 
 
