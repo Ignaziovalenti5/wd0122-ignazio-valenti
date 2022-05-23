00ca2afe00c8a9f5e19c4ca507a83f65920e3b90
Ignaziovalenti5 <ignazio.valenti.87@gmail.com>
2022-05-23 16:46:14 +0200

    m3 w1 d1

---
+++ m3/w1/d1/start/script.js
@@ -0,0 +1,29 @@
+var targetRandom = document.querySelector('#target-random');
+var result = document.querySelector('#result');
+var resultDesc = document.querySelector('#result-desc');
+var input1 = document.querySelector('#player-1');
+var input2 = document.querySelector('#player-2');
+var button = document.querySelector('#confronta');
+button.addEventListener('click', function (e) {
+    e.preventDefault();
+    var casualNumber = Math.floor(Math.random() * (100 - 1) + 1);
+    targetRandom.innerText = String(casualNumber);
+    var condition1 = Math.abs((casualNumber - Number(input1.value)));
+    var condition2 = Math.abs((casualNumber - Number(input2.value)));
+    if (condition1 < condition2 && condition1 == 0) {
+        result.innerText = 'Giocatore 1';
+        resultDesc.innerText = 'Il giocatore 1 ha azzeccato il numero!';
+    }
+    else if (condition1 < condition2) {
+        result.innerText = 'Giocatore 1';
+        resultDesc.innerText = 'Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più!';
+    }
+    if (condition1 > condition2 && condition2 == 0) {
+        result.innerText = 'Giocatore 2';
+        resultDesc.innerText = 'Il giocatore 2 ha azzeccato il numero!';
+    }
+    else if (condition1 > condition2) {
+        result.innerText = 'Giocatore 2';
+        resultDesc.innerText = 'Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più!';
+    }
+});
---
+++ m3/w1/d1/start/script.ts
@@ -0,0 +1,38 @@
+
+let targetRandom:HTMLElement|null = document.querySelector('#target-random');
+let result:HTMLElement|null = document.querySelector('#result');
+let resultDesc:HTMLElement|null = document.querySelector('#result-desc');
+let input1:HTMLInputElement|null = document.querySelector('#player-1')
+let input2:HTMLInputElement|null = document.querySelector('#player-2')
+let button:HTMLButtonElement|null = document.querySelector('#confronta');
+
+
+button!.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    let casualNumber:number = Math.floor(Math.random() * (100 -1) + 1);
+
+    targetRandom!.innerText = String(casualNumber);
+
+    let condition1:number = Math.abs((casualNumber - Number(input1!.value)))
+    let condition2:number = Math.abs((casualNumber - Number(input2!.value)))
+    
+    if (condition1 < condition2 && condition1 == 0) {
+        result!.innerText = 'Giocatore 1'
+        resultDesc!.innerText = 'Il giocatore 1 ha azzeccato il numero!'
+    }else if(condition1 < condition2){
+        result!.innerText = 'Giocatore 1'
+        resultDesc!.innerText = 'Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più!'
+    }
+
+    if (condition1 > condition2 && condition2 == 0) {
+        result!.innerText = 'Giocatore 2'
+        resultDesc!.innerText = 'Il giocatore 2 ha azzeccato il numero!'
+    }else if(condition1 > condition2){
+        result!.innerText = 'Giocatore 2'
+        resultDesc!.innerText = 'Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più!'
+    }
+    
+})
+
+	
