01133d025d50cde4c4c7660e49062f0dc9d7b5bc
Ignaziovalenti5 <ignazio.valenti.87@gmail.com>
2022-05-23 19:40:59 +0200

    m3 w1 d1

--- m3/w1/d1/start/script.js
+++ m3/w1/d1/start/script.js
@@ -4,30 +4,74 @@ var resultDesc = document.querySelector('#result-desc');
 var input1 = document.querySelector('#player-1');
 var input2 = document.querySelector('#player-2');
 var button = document.querySelector('#confronta');
+var tableHistory = document.querySelector('#history');
+var titoloResult = document.querySelector('#titolo-result');
+var i = 0;
 button.addEventListener('click', function (e) {
     e.preventDefault();
+    i++;
     var casualNumber = Math.floor(Math.random() * (100 - 1) + 1);
     targetRandom.innerText = String(casualNumber);
     var condition1 = Math.abs((casualNumber - Number(input1.value)));
     var condition2 = Math.abs((casualNumber - Number(input2.value)));
-    if (input1.value == input2.value) {
-        result.innerText = 'Pari!';
-        resultDesc.innerText = 'Entrambi i giocatori hanno scelto lo stesso numero!';
-    }
-    if (condition1 == 0) {
-        result.innerText = 'Giocatore 1';
+    if (condition1 == 0 && !(input2.value == '')) {
+        titoloResult.innerHTML = 'Ha vinto il:';
+        result.innerText = 'Giocatore 1 😎';
         resultDesc.innerText = 'Il giocatore 1 ha azzeccato il numero!';
     }
-    else if (condition1 < condition2) {
+    else if (condition1 < condition2 && !(input1.value == '' || input2.value == '')) {
+        titoloResult.innerHTML = 'Ha vinto il:';
         result.innerText = 'Giocatore 1';
         resultDesc.innerText = 'Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più!';
     }
-    if (condition2 == 0) {
-        result.innerText = 'Giocatore 2';
+    if (condition2 == 0 && !(input1.value == '')) {
+        titoloResult.innerHTML = 'Ha vinto il:';
+        result.innerText = 'Giocatore 2 😎';
         resultDesc.innerText = 'Il giocatore 2 ha azzeccato il numero!';
     }
-    else if (condition1 > condition2) {
+    else if (condition1 > condition2 && !(input1.value == '')) {
+        titoloResult.innerHTML = 'Ha vinto il:';
         result.innerText = 'Giocatore 2';
         resultDesc.innerText = 'Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più!';
+    }
+    if (input1.value == input2.value) {
+        result.innerText = 'Pareggio!';
+        resultDesc.innerText = 'Entrambi i giocatori hanno scelto lo stesso numero!';
+    }
+    if (input1.value == '' || input2.value == '') {
+        i--;
+        titoloResult.innerHTML = '';
+        targetRandom.innerText = '';
+        result.innerText = 'Attenzione!';
+        resultDesc.innerText = 'Entrambi i giocatori devono scegliere un numero!';
+    }
+    else {
+        button.classList.add('d-none');
+        var playerChoise1 = input1.value;
+        var playerChoise2 = input2.value;
+        var winner = result.innerText;
+        setTimeout(function () {
+            button.classList.remove('d-none');
+            input1.value = '';
+            input2.value = '';
+            result.innerText = '';
+            resultDesc.innerText = '';
+            targetRandom.innerText = '';
+            titoloResult.innerHTML = '';
+        }, 2000);
+        var tr = document.createElement('tr');
+        tr.classList.add('border-bottom');
+        var tdNum = document.createElement('td');
+        tdNum.innerText = String(i);
+        var tdPlayer1 = document.createElement('td');
+        tdPlayer1.innerText = playerChoise1;
+        var tdPlayer2 = document.createElement('td');
+        tdPlayer2.innerText = playerChoise2;
+        var tdRandom = document.createElement('td');
+        tdRandom.innerText = String(casualNumber);
+        var tdWinner = document.createElement('td');
+        tdWinner.innerText = winner;
+        tr.append(tdNum, tdPlayer1, tdPlayer2, tdRandom, tdWinner);
+        tableHistory.append(tr);
     }
 });
--- m3/w1/d1/start/script.ts
+++ m3/w1/d1/start/script.ts
@@ -4,38 +4,90 @@ let result:HTMLElement|null = document.querySelector('#result');
 let resultDesc:HTMLElement|null = document.querySelector('#result-desc');
 let input1:HTMLInputElement|null = document.querySelector('#player-1')
 let input2:HTMLInputElement|null = document.querySelector('#player-2')
-let button:HTMLButtonElement|null = document.querySelector('#confronta');
+let button = document.querySelector('#confronta');
+let tableHistory = document.querySelector('#history')
+let titoloResult = document.querySelector('#titolo-result')
 
+
+let i = 0
 
 button!.addEventListener('click', function (e) {
     e.preventDefault();
+    i++
     
     let casualNumber:number = Math.floor(Math.random() * (100 -1) + 1);
-
+    
     targetRandom!.innerText = String(casualNumber);
-
+    
     let condition1:number = Math.abs((casualNumber - Number(input1!.value)))
     let condition2:number = Math.abs((casualNumber - Number(input2!.value)))
-
-    if(input1!.value == input2!.value){
-        result!.innerText = 'Pari!'
-        resultDesc!.innerText = 'Entrambi i giocatori hanno scelto lo stesso numero!'
-    }
     
-    if (condition1 == 0) {
-        result!.innerText = 'Giocatore 1'
+    if (condition1 == 0 && !(input2!.value == '')) {
+        titoloResult!.innerHTML = 'Ha vinto il:'
+        result!.innerText = 'Giocatore 1 😎'
         resultDesc!.innerText = 'Il giocatore 1 ha azzeccato il numero!'
-    }else if(condition1 < condition2){
+    }else if(condition1 < condition2 && !(input1!.value == '' || input2!.value == '')){
+        titoloResult!.innerHTML = 'Ha vinto il:'
         result!.innerText = 'Giocatore 1'
         resultDesc!.innerText = 'Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più!'
     }
-
-    if (condition2 == 0) {
-        result!.innerText = 'Giocatore 2'
+    
+    if (condition2 == 0 && !(input1!.value == '')) {
+        titoloResult!.innerHTML = 'Ha vinto il:'
+        result!.innerText = 'Giocatore 2 😎'
         resultDesc!.innerText = 'Il giocatore 2 ha azzeccato il numero!'
-    }else if(condition1 > condition2){
+    }else if(condition1 > condition2 && !(input1!.value == '')){
+        titoloResult!.innerHTML = 'Ha vinto il:'
         result!.innerText = 'Giocatore 2'
         resultDesc!.innerText = 'Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più!'
+    }
+
+    if(input1!.value == input2!.value){
+        result!.innerText = 'Pareggio!'
+        resultDesc!.innerText = 'Entrambi i giocatori hanno scelto lo stesso numero!'
+    }
+    
+    if(input1!.value == '' || input2!.value == ''){
+        i--
+        titoloResult!.innerHTML = ''
+        targetRandom!.innerText = ''
+        result!.innerText = 'Attenzione!'
+        resultDesc!.innerText = 'Entrambi i giocatori devono scegliere un numero!'
+    }else{
+        button!.classList.add('d-none')
+
+        let playerChoise1 = input1!.value
+        let playerChoise2 = input2!.value
+        let winner = result!.innerText
+    
+        setTimeout(() => {
+            button!.classList.remove('d-none')
+            input1!.value = ''
+            input2!.value = ''
+            result!.innerText = ''
+            resultDesc!.innerText = ''
+            targetRandom!.innerText = ''
+            titoloResult!.innerHTML = ''
+        }, 2000);
+
+            let tr = document.createElement('tr')
+            tr.classList.add('border-bottom')
+            
+            let tdNum = document.createElement('td')
+            tdNum!.innerText = String(i)
+            let tdPlayer1 = document.createElement('td')
+            tdPlayer1!.innerText = playerChoise1
+            let tdPlayer2 = document.createElement('td')
+            tdPlayer2!.innerText = playerChoise2
+            let tdRandom = document.createElement('td')
+            tdRandom!.innerText = String(casualNumber)
+            let tdWinner = document.createElement('td')
+            tdWinner!.innerText = winner
+
+            tr.append(tdNum, tdPlayer1, tdPlayer2, tdRandom, tdWinner)
+            tableHistory!.append(tr)
+            
+
     }
     
 })
